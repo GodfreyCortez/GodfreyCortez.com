0140e3e116ccf3647597686c1188d043ff661091
Godfrey <GodfreyCortez@outlook.com>
2019-04-19 21:59:27 -0400

    Updated Carousel to transition smoothly and added pics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,18 +2,12 @@ const $ = require('jquery')
 require('bootstrap')
 
 // Image URLs
-const headerImages = [
+/* const headerImages = [
   '/resources/jabez-samuel-1091148-unsplash-min.jpg',
   '/resources/marcus-dall-col-1321486-unsplash-min.jpg',
   '/resources/gaspar-zaldo-1362111-unsplash-min.jpg'
-]
+] */
 
 $(document).ready(() => {
-  let imageCounter = 0
-  const changeImage = () => {
-    imageCounter = (imageCounter === headerImages.length - 1) ? 0 : imageCounter + 1
-    $('.jumbotron').css('background-image', `url("${headerImages[imageCounter]}"`)
-  }
 
-  setInterval(changeImage, 3000)
 })
