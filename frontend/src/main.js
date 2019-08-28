1e44b3de1ce5e971c0494d14ec083085d155c6c1
Godfrey Cortez <godfreycortez@outlook.com>
2019-08-28 11:29:20 -0400

    updated package-lock, added new scotia logo

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,1 @@
-const $ = require('jquery')
-
-// Image URLs
-/* const headerImages = [
-  '/resources/jabez-samuel-1091148-unsplash-min.jpg',
-  '/resources/marcus-dall-col-1321486-unsplash-min.jpg',
-  '/resources/gaspar-zaldo-1362111-unsplash-min.jpg'
-] */
-
-$(document).ready(() => {
-
-})
+require('bootstrap')
